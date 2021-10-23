74b6eebfb29e6bbb34c5327949dd57996d52d937
jstawinski <jjs463@cornell.edu>
2021-10-22 20:52:32 -0400

    add db specs

--- data/db.ml
+++ data/db.ml
@@ -1,54 +1,64 @@
-(* This file is some function defns that Ria made, I'm not sure if it
-   will be helpful to use those or if it would be easier to just start
-   over. Regaurdless, here are the functions we need: for every field in
-   the db.json file (i.e. username, password, new messages, etc.) we
-   need: 1) a function to read the value of that field based on a
-   session ID 2) a function to write the value of that field based on a
-   user ID.
+open Yojson.Basic.Util
 
-   Additionally we need a function to add a user, which means add a user
-   with every field to the json file, using a given username and
-   password. We need a function to delete new messages based on a
-   session id. We need a function to write a session ID value based on a
-   username*)
+type username = string
 
-type user = {
-  id : string;
-  pass : string;
-  token : string;
+type session_id = string
+
+type msg = {
+  sender : string;
+  msg : string;
 }
 
-type chat = {
-  id : string;
-  user_list : user list;
-  messages : (user * string) list;
-}
+let file = "db.json"
 
-type application = { chats : chat list }
+(* [get_uname id] returns the username associated with the session id
+   [id]. Raises "DNE" if that session id does not exist. *)
+let get_uname (id : string) : string = failwith "unimplemented"
 
-(** function that retrives the user's name *)
-let get_user_id user = user.id
+(* [get_password id] returns the password associated with the session id
+   [id]. Raises "DNE" if that session id does not exist.*)
+let get_password (id : string) : string = failwith "unimplemented"
 
-(** function that retrives the user's password *)
-let get_user_pass user = user.pass
+(* [get_new_msgs id] returns a list of the new messages associated with
+   the session id [id]. Raises "DNE" if that session id does not
+   exist.*)
+let get_new_msgs (id : string) : msg list = failwith "unimplemented"
 
-(** function that retrives the user's token *)
-let get_user_token user = user.token
+(* [put_uname id] replaces the username in the database file with the
+   specified username [new_uname] associated with the session id [id].
+   Raises "DNE" if that session id does not exist. *)
+let put_uname (id : string) (new_uname : string) =
+  failwith "unimplemented"
 
-(** function that gets all chats *)
-let get_chats app = app.chats
+(* [put_passwd id] replaces the password in the database file with the
+   specified password [new_passwd] associated with the session id [id].
+   Raises "DNE" if that session id does not exist. *)
+let put_passwd (id : string) (new_passwd : string) =
+  failwith "unimplemented"
 
-(** function that gets a specific chat by id *)
-let rec get_chat_by_id chats id =
-  match chats with
-  | h :: t -> if h.id = id then Some h else get_chat_by_id t id
-  | _ -> None
+(* [put_msg reciever new_msg] adds the msg [new_msg] to the new messages
+   array associated with username [reciever] in the database file.
+   Raises "DNE" if that username does not exist. *)
+let put_msg (reciever : username) (new_msg : msg) =
+  failwith "unimplemented"
 
-(** function that gets the user list for a chat*)
-let get_chat_user_list chat = chat.user_list
+(* [put_id uname] replaces the session id in the database file with the
+   specified session id [id] associated with the username [uname].
+   Raises "DNE" if that username does not exist. *)
+let put_id (id : string) (uname : string) = failwith "unimplemented"
 
-(** function that checks if a user is in the chat*)
-let rec check_chat_user user_list user =
-  match user_list with
-  | h :: t -> if h.id = user.id then true else check_chat_user t user
-  | _ -> false
+(* [add_user id uname passwd] adds a user to the database file with
+   session id [id], username [uname], password [passwd], and an empty
+   new messages array. This function will be called when a client
+   creates an account with the service. *)
+let add_user (id : string) (uname : string) (passwd : string) =
+  failwith "unimplemented"
+
+(* [delete_msg uname msg] deletes the message [msg] associated with
+   username [uname] from the database file. Raises "DNE" if that
+   username does not exist. *)
+let delete_msg (uname : string) (msg : msg) = failwith "unimplemented"
+
+(* [list_unames] returns a list of all the usernames in the database
+   file. *)
+let list_unames : string list = failwith "unimplemented"
