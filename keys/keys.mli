c1f0709b19ae139dcb7e69d9125dc4afe7802b26
Nadav <nle9@cornell.edu>
2021-10-24 16:50:07 -0400

    Implemented Diffie-Hellman key exchange functions 1st-iteration. Encryption/Decryption not implemented yet.

--- keys/keys.mli
+++ keys/keys.mli
@@ -11,32 +11,33 @@ val get_public_key : keys -> string
 
 val dh_pub_info : unit -> pub_info
 (**[dh_pub_info ()] is a new public info used for diffie-hellman key
-   exchange and encryption*)
+   exchange and encryption.*)
 
 val create_dh_keys : pub_info -> keys
-(**[create_dh_keys p] is a new private key public for diffie-hellman key
-   exchange before the shared key is generated.*)
+(**[create_dh_keys p] is a new private key, public key pair for
+   diffie-hellman key exchange before the shared key is generated.*)
 
 val create_dh_shared_key : keys -> string -> pub_info -> keys
 (**[create_dh_keys k s p] is the keys [k] updated with the shared key
    generated using [s] as the partner's public key and p as the public
    info.*)
 
-val encrypt_dh : keys -> bytes -> bytes
-(**[encrypt_dh k b] is the bytes [b] encrypted using the diffie-hellman
-   shared private key in [k]*)
+val encrypt_dh : keys -> bytes -> pub_info -> bytes
+(**[encrypt_dh k b p] is the bytes [b] encrypted using the
+   diffie-hellman shared private key in [k] and the public mod from [p].*)
 
-val decrypt_dh : keys -> bytes -> bytes
+val decrypt_dh : keys -> bytes -> pub_info -> bytes
 (**[decrypt_dh k b] is the bytes [b] decrypted using the diffie-hellman
-   shared private key in [k]*)
+   shared private key in [k] and the public mod from [p].*)
 
 val create_rsa_keys : unit -> keys
 (**[create_rsa_keys ()] is a new public key, private pair used for rsa
-   encryption*)
+   encryption.*)
 
 val encrypt_rsa : string -> bytes -> bytes
-(**[encrypt_rsa s b] is the bytes [b] encrypted using the public key [s]*)
+(**[encrypt_rsa s b] is the bytes [b] encrypted using the public key
+   [s].*)
 
 val decrypt_rsa : keys -> bytes -> bytes
 (**[decrypt_rsa k b] is the bytes [b] decrypted using the private key
-   pair [k]*)
+   pair [k].*)
