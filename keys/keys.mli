b67bffeea13559e692627dbeebcff8cee7785e70
Nadav <nle9@cornell.edu>
2021-10-27 13:27:57 -0400

    -Changed keys.mli specs: totally different functions for DH and RSA -Added RSA test

--- keys/keys.mli
+++ keys/keys.mli
@@ -1,48 +1,59 @@
-type keys = {
+type dh_keys = {
   private_key : Z.t * Z.t;
   public_key : Z.t;
 }
-(**[keys] is the type representing a private key, public key pair.*)
+(**[dh_keys] is the type representing a Diffie-Hellman private key,
+   public key pair.*)
 
 type pub_info
 (**[pub_info] is the type representing the public info used to generate
    keys.*)
 
-val get_public_key : keys -> string
-(**[get_public_key k] is the public key of the public key, private key
-   pair [k] as a string.*)
+val dh_get_public_key : dh_keys -> string
+(**[dh_get_public_key k] is the public key of the Diffie-Hellamn public
+   key, private key pair [k] as a string.*)
 
 val dh_pub_info : unit -> pub_info
 (**[dh_pub_info ()] is a new public info used for diffie-hellman key
    exchange and encryption.*)
 
-val create_dh_keys : pub_info -> keys
+val create_dh_keys : pub_info -> dh_keys
 (**[create_dh_keys p] is a new private key, public key pair for
    diffie-hellman key exchange before the shared key is generated.*)
 
-val create_dh_shared_key : keys -> string -> pub_info -> keys
+val create_dh_shared_key : dh_keys -> string -> pub_info -> dh_keys
 (**[create_dh_keys k s p] is the keys [k] updated with the shared key
    generated using [s] as the partner's public key and p as the public
    info.*)
 
-val encrypt_dh : keys -> string -> string
-(**[encrypt_dh k s] is the string [s] encrypted using the diffie-hellman
-   shared private key in [k]. Requires: [k] has had a shared key
-   generated using [create_dh_shared_key].*)
+val encrypt_dh : dh_keys -> string -> string list
+(**[encrypt_dh k s] is the string [s] encrypted in blocks using the
+   diffie-hellman shared private key in [k]. Requires: [k] has had a
+   shared key generated using [create_dh_shared_key].*)
 
-val decrypt_dh : keys -> string -> string
-(**[decrypt_dh k s] is the string [s] decrypted using the diffie-hellman
-   shared private key in [k]. Requires: [k] has had a shared key
-   generated using [create_dh_shared_key].*)
+val decrypt_dh : dh_keys -> string list -> string
+(**[decrypt_dh k s] is the list of encrypted string blocks [s] decrypted
+   using the diffie-hellman shared private key in [k]. Requires: [k] has
+   had a shared key generated using [create_dh_shared_key].*)
 
-val create_rsa_keys : unit -> keys
+type rsa_keys
+(**[dh_keys] is the type representing an RSA private key, public key
+   pair.*)
+
+val rsa_get_public_key : rsa_keys -> string * string
+(**[rsa_get_public_key k] is the RSA public key pair of the public key,
+   private key pair [k] as a string tuple.*)
+
+val create_rsa_keys : unit -> rsa_keys
 (**[create_rsa_keys ()] is a new public key, private pair used for rsa
    encryption.*)
 
-val encrypt_rsa : string -> bytes -> bytes
-(**[encrypt_rsa s b] is the bytes [b] encrypted using the public key
-   [s].*)
+val encrypt_rsa : string * string -> string -> string list
+(**[encrypt_rsa k s] is list of encrypted string blocks string [s]
+   encrypted using the RSA public key pair in [k] into blocks smaller
+   than the size of the public modulus. Requires: [k] is public key
+   generated using [create_rsa_keys]*)
 
-val decrypt_rsa : keys -> bytes -> bytes
-(**[decrypt_rsa k b] is the bytes [b] decrypted using the private key
-   pair [k].*)
+val decrypt_rsa : rsa_keys -> string list -> string
+(**[decrypt_rsa k s] is the string [s] decrypted using the private key
+   in [k].*)
--- test/main.ml
+++ test/main.ml
@@ -12,11 +12,11 @@ let keys_tests =
     (let p = dh_pub_info () in
      let k1 = create_dh_keys p in
      let k2 = create_dh_keys p in
-     let pub1 = get_public_key k1 in
-     let pub2 = get_public_key k2 in
+     let pub1 = dh_get_public_key k1 in
+     let pub2 = dh_get_public_key k2 in
      let k1 = create_dh_shared_key k1 pub2 p in
      let k2 = create_dh_shared_key k2 pub1 p in
-     test "Shared key is the same"
+     test "DH Shared key is the same"
        (Z.to_string (snd k1.private_key))
        (Z.to_string (snd k2.private_key)))
     50
@@ -24,13 +24,21 @@ let keys_tests =
       (let p = dh_pub_info () in
        let k1 = create_dh_keys p in
        let k2 = create_dh_keys p in
-       let pub1 = get_public_key k1 in
-       let pub2 = get_public_key k2 in
+       let pub1 = dh_get_public_key k1 in
+       let pub2 = dh_get_public_key k2 in
        let k1 = create_dh_shared_key k1 pub2 p in
        let k2 = create_dh_shared_key k2 pub1 p in
        let s = string_of_int (Random.bits ()) in
-       test "encrypt then decrypt keeps a string the same"
-         (decrypt_dh k2 (encrypt_dh k1 s))
+       test "DH encrypt then decrypt keeps a string the same"
+         (s |> encrypt_dh k1 |> decrypt_dh k2)
+         s)
+      50
+  @ test_many
+      (let k = create_rsa_keys () in
+       let pub = rsa_get_public_key k in
+       let s = string_of_int (Random.bits ()) in
+       test "RSA encrypt then decrypt keeps a string the same"
+         (s |> encrypt_rsa pub |> decrypt_rsa k)
          s)
       50
 
