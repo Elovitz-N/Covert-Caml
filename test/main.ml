9ceef9a0b9f1dea14a2adc57f9e2319820247424
Nadav <nle9@cornell.edu>
2021-10-26 23:23:04 -0400

    Changed the test directory in preparation for testing Keys.

--- test/main.ml
+++ test/main.ml
@@ -1,25 +1,12 @@
 open OUnit2
 open Yojson.Basic.Util
-open Database
-open Jsondb
+open Keys
 
-let users = Yojson.Basic.from_file "data/users.json"
-let users_t = from_json users
+let test (name : string) input expected_output : test =
+  name >:: fun _ -> assert_equal expected_output input
 
-let users_test
-    (name : string)
-    (adv : Jsondb.t)
-    (expected_output : string) : test =
-  name >:: fun _ -> assert_equal expected_output (first_user adv)
+let keys_tests = []
 
-  let database_tests =
-    [
-      users_test "Get the first user" users_t
-        "test_data";
-    ]
+let suite = "Testing" >::: List.flatten [ keys_tests ]
 
-    let suite =
-      "Testing Database"
-      >::: List.flatten [ database_tests;]
-    
-    let _ = run_test_tt_main suite
+let _ = run_test_tt_main suite
